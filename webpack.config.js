2a878cf3b8ea7e4cae4201bac7053766b0c99b4c
maxsalo17 <makssalo13@gmail.com>
2020-09-29 14:23:58 +0300

    refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { mainModule } = require('process');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: "development",
