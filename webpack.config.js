b36e9b780731d529a378acff56c344049916599a
maxsalo17 <makssalo13@gmail.com>
2020-09-29 14:09:35 +0300

    change html by pug

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { mainModule } = require('process');
+
+module.exports = {
+    mode: "development",
+    entry: "./src/main.js",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "main.js"
+    },
+    devServer:{
+        host: 'localhost',
+        port: 3000,
+        stats: 'errors-only',
+        open: true
+    },
+    module:{
+        rules: [
+            {
+                test: /\.pug/,
+                use: ['pug-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/pug/index.pug"
+        })
+    ]
+}
